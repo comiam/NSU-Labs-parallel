e48e6941bb806f080d4d295b0c1e36b437072cbe
Maxim <maxim.bolshim@yandex.ru>
2020-05-05 18:19:12 +0700

    Datatype size fix Adaptaition code for MPE(rewrite on C syntax)

--- Matrix_Multiply_MPI/main.cpp
+++ Matrix_Multiply_MPI/main.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <cerrno>
 #include <mpi.h>
+#include <cmath>
+//#define DEBUG
 
 #define DIMENSIONS 2
 #define X 0
@@ -10,17 +12,18 @@
 void init_commutators(MPI_Comm *grid_comm, MPI_Comm *rows_comm, MPI_Comm *col_comm, int *coords, int *dims);
 void distribute_matrices(const double *matrixA, const double *matrixB, double *partA, double *partB,
         MPI_Comm row_comm, MPI_Comm col_comm, const int coords[DIMENSIONS], const int dims[DIMENSIONS], int n1, int n2, int n3);
-void build_result_matrix(const double *partC, double *C, MPI_Comm grid_comm, const int dims[DIMENSIONS], int coords[DIMENSIONS], int n1, int n3, int comm_size);
+void build_result_matrix(double *partC, double *C, MPI_Comm grid_comm, const int dims[DIMENSIONS], int coords[DIMENSIONS], int n1, int n3, int comm_size);
 void fill_matrix(double *matrix, int row, int col, double value);
 void matrix_mul(const double *A, const double *B, double *C, int rowsA, int colA, int colB);
-void init_matrices(double **A, double **B, double **C, int n1, int n2, int n3);
+void init_matrices(double **A, double **B, double **C, double **C_test, int n1, int n2, int n3, char **argv, int argc);
 void print_matrix(const double *matrix, int n1, int n2);
-void free_all(double *A, double *B, double *C, double *partA, double *partB, double *partC, MPI_Comm *grid_comm, MPI_Comm *col_comm, MPI_Comm *row_comm, int rank);
+void free_all(double *A, double *B, double *C, double *C_test, double *partA, double *partB, double *partC, MPI_Comm *grid_comm, MPI_Comm *col_comm, MPI_Comm *row_comm, int rank);
+double compare_matrices(double *A, double *B, int n1, int n3);
 
 int main(int argc, char **argv)
 {
     MPI_Init(&argc, &argv);
-    
+
     int dims[DIMENSIONS] = {0, 0};
     int coords[DIMENSIONS];
     int size;
@@ -34,9 +37,9 @@ int main(int argc, char **argv)
     init_commutators(&grid_comm, &row_comm, &col_comm, coords, dims);
     MPI_Comm_rank(grid_comm, &rank);
 
-    int n1 = (int)strtol(argv[1], nullptr, 10);
-    int n2 = (int)strtol(argv[2], nullptr, 10);
-    int n3 = (int)strtol(argv[3], nullptr, 10);
+    int n1 = (int)strtol(argv[1], NULL, 10);
+    int n2 = (int)strtol(argv[2], NULL, 10);
+    int n3 = (int)strtol(argv[3], NULL, 10);
 
     if(!rank && errno == ERANGE)
     {
@@ -48,37 +51,43 @@ int main(int argc, char **argv)
         printf("My rank = %d and my coords is %d %d\n", rank, coords[X], coords[Y]);
 #endif
 
-    double *A = nullptr;
-    double *B = nullptr;
-    double *C = nullptr;
+    double *A = NULL;
+    double *B = NULL;
+    double *C = NULL;
+    double *C_test = NULL;
 
-    auto *partA = new double[n1 * n2 / dims[1]];
-    auto *partB = new double[n2 * n3 / dims[0]];
-    auto *partC = new double[n1 * n3 / (dims[X] * dims[Y])];
+    double *partA = (double*)malloc(sizeof(double) * n1 * n2 / dims[1]);
+    double *partB = (double*)malloc(sizeof(double) * n2 * n3 / dims[0]);
+    double *partC = (double*)malloc(sizeof(double) * n1 * n3 / (dims[X] * dims[Y]));
 
     if (!rank)
-        init_matrices(&A, &B, &C, n1, n2, n3);
+        init_matrices(&A, &B, &C, &C_test, n1, n2, n3, argv, argc);
     
     distribute_matrices(A, B, partA, partB, row_comm, col_comm, coords, dims, n1, n2, n3);
     matrix_mul(partA, partB, partC, n1 / dims[Y], n2, n3 / dims[X]);
     build_result_matrix(partC, C, grid_comm, dims, coords, n1, n3, size);
 
-    if (!rank)
+    if (!rank && argc != 7)
         print_matrix(C, n1, n3);
+    else if (!rank)
+        printf("Error compare: %lf\n", compare_matrices(C_test, C, n1, n3));
 
-    free_all(A, B, C, partA, partB, partC, &grid_comm, &col_comm, &row_comm, rank);
+    free_all(A, B, C, C_test, partA, partB, partC, &grid_comm, &col_comm, &row_comm, rank);
+
     MPI_Finalize();
 
     return EXIT_SUCCESS;
 }
 
-void free_all(double *A, double *B, double *C, double *partA, double *partB, double *partC, MPI_Comm *grid_comm, MPI_Comm *col_comm, MPI_Comm *row_comm, int rank)
+void free_all(double *A, double *B, double *C, double *C_test, double *partA, double *partB, double *partC, MPI_Comm *grid_comm, MPI_Comm *col_comm, MPI_Comm *row_comm, int rank)
 {
     if (!rank)
     {
         free(A);
         free(B);
         free(C);
+        if(C_test)
+            free(C_test);
     }
     free(partA);
     free(partB);
@@ -89,12 +98,14 @@ void free_all(double *A, double *B, double *C, double *partA, double *partB, dou
     MPI_Comm_free(row_comm);
 }
 
-void build_result_matrix(const double *partC, double *C, MPI_Comm grid_comm, const int dims[DIMENSIONS], int coords[DIMENSIONS], int n1, int n3, int comm_size)
+void build_result_matrix(double *partC, double *C, MPI_Comm grid_comm, const int dims[DIMENSIONS], int coords[DIMENSIONS], int n1, int n3, int comm_size)
 {
-    int *recvCounts = new int[comm_size];
-    int *displs = new int[comm_size];
+    int *recvCounts = (int*)malloc(sizeof(int) * comm_size);
+    int *displs = (int*)malloc(sizeof(int) * comm_size);
 
 #ifdef DEBUG
+    int rank;
+    MPI_Comm_rank(grid_comm, &rank);
     if (!rank)
     {
         printf("dims %d %d \n", dims[0], dims[1]);
@@ -108,7 +119,7 @@ void build_result_matrix(const double *partC, double *C, MPI_Comm grid_comm, con
     MPI_Type_vector(n1 / dims[Y], n3 / dims[X], n3, MPI_DOUBLE, &recv_vector_t);
     MPI_Type_commit(&recv_vector_t);
 
-    MPI_Type_create_resized(recv_vector_t, 0, n3 / dims[X] * sizeof(MPI_DOUBLE), &resized_recv_vector_t);
+    MPI_Type_create_resized(recv_vector_t, 0, n3 / dims[X] * sizeof(double), &resized_recv_vector_t);
     MPI_Type_commit(&resized_recv_vector_t);
 
     int typeSizeBytes;
@@ -125,7 +136,7 @@ void build_result_matrix(const double *partC, double *C, MPI_Comm grid_comm, con
         recvCounts[rank_i] = 1;
 
         MPI_Cart_coords(grid_comm, rank_i, DIMENSIONS, coords);
-        displs[rank_i] = coords[Y] * dims[X] * (int)typeSize + coords[X] * 2;
+        displs[rank_i] = dims[X] * (n1 / dims[Y]) * coords[Y] + coords[X];
 
 #ifdef DEBUG
         if (!rank)
@@ -155,22 +166,52 @@ void build_result_matrix(const double *partC, double *C, MPI_Comm grid_comm, con
     MPI_Type_free(&send_vector_t);
 }
 
-void init_matrices(double **A, double **B, double **C, int n1, int n2, int n3)
+void init_matrices(double **A, double **B, double **C, double **C_test, int n1, int n2, int n3, char **argv, int argc)
 {
-    *A = new double[n1 * n2];
-    *B = new double[n2 * n3];
-    *C = new double[n1 * n3];
+    *A = (double*)malloc(sizeof(double) * n1 * n2);
+    *B = (double*)malloc(sizeof(double) * n2 * n3);
+    *C = (double*)malloc(sizeof(double) * n1 * n3);
 
-    fill_matrix(*A, n1, n2, 2);
-    fill_matrix(*B, n2, n3, 2);
+    if(argc == 7)
+    {
+        *C_test = (double*)malloc(sizeof(double) * n1 * n3);
 
-    printf("matrix A:\n");
-    print_matrix(*A, n1, n2);
+        FILE* A_f = fopen(argv[4], "rb");
+        FILE* B_f = fopen(argv[5], "rb");
+        FILE* C_f = fopen(argv[6], "rb");
 
-    printf("matrix B:\n");
-    print_matrix(*B, n2, n3);
+        float a;
+        for (int i = 0; i < n1 * n2; ++i)
+        {
+            fread(((void *) &a), sizeof(float), 1, A_f);
+            (*A)[i] = a;
+        }
+        for (int i = 0; i < n2 * n3; ++i)
+        {
+            fread(((void *) &a), sizeof(float), 1, B_f);
+            (*B)[i] = a;
+        }
+        for (int i = 0; i < n1 * n3; ++i)
+        {
+            fread(((void *) &a), sizeof(float), 1, C_f);
+            (*C_test)[i] = a;
+        }
+        fclose(A_f);
+        fclose(B_f);
+        fclose(C_f);
+    } else
+    {
+        fill_matrix(*A, n1, n2, 2);
+        fill_matrix(*B, n2, n3, 2);
 
-    printf("-----------------------------RESULT-----------------------------\n");
+        printf("matrix A:\n");
+        print_matrix(*A, n1, n2);
+
+        printf("matrix B:\n");
+        print_matrix(*B, n2, n3);
+
+        printf("-----------------------------RESULT-----------------------------\n");
+    }
 }
 
 void init_commutators(MPI_Comm *grid_comm, MPI_Comm *rows_comm, MPI_Comm *col_comm, int *coords, int *dims)
@@ -254,4 +295,13 @@ void print_matrix(const double *matrix, int n1, int n2)
 
         printf("\n");
     }
+}
+
+double compare_matrices(double *A, double *B, int n1, int n3)
+{
+    double error = 0;
+    for(int i = 0; i < n1;i++)
+        for(int j = 0; j < n3;j++)
+            error += fabs(A[i * n3 + j] - B[i * n3 + j]) * fabs(A[i * n3 + j] - B[i * n3 + j]);
+    return error;
 }
